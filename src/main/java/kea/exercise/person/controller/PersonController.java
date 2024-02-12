4b81d86710e9a97b53585969f97516d2507cf33d
Maltemork <malte.sorensen@protonmail.com>
2024-02-12 11:17:52 +0100

    fixed route for specific persons

--- src/main/java/kea/exercise/person/controller/PersonController.java
+++ src/main/java/kea/exercise/person/controller/PersonController.java
@@ -1,2 +1,35 @@
-package kea.exercise.person.controller;public class PersonController {
+package kea.exercise.person.controller;
+
+import kea.exercise.person.model.Person;
+import kea.exercise.person.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.time.LocalDate;
+import java.util.Optional;
+
+@RestController
+public class PersonController {
+    private final PersonRepository personRepository;
+
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+    @GetMapping("/persons")
+    public List<Person> getAllPersons() {
+        List<Person> persons = personRepository.findAll();
+
+        return persons;
+    }
+
+    @GetMapping("/persons/{id}")
+    public Person getPerson(@PathVariable int id) {
+        Optional<Person> person = personRepository.findById(id);
+
+        return person.orElse(null);
+    }
 }
--- src/main/java/kea/exercise/person/repository/PersonRepository.java
+++ src/main/java/kea/exercise/person/repository/PersonRepository.java
@@ -1,2 +1,8 @@
-package kea.exercise.person.repository;public interface PersonRepository {
+package kea.exercise.person.repository;
+
+import kea.exercise.person.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, Integer> {
+
 }
